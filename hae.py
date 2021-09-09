1a0478a4300682ca9db2b39544167160dcec06c2
Lewbrino <joespider885@yahoo.com>
2021-09-09 10:11:34 +0430

    Update hae.py

--- hae.py
+++ hae.py
@@ -36,6 +36,7 @@ from copy import deepcopy
 import pickle
 import itertools
 from time import time
+from rqdm import tqdm
 
 from cqa_supports import *
 from cqa_flags import FLAGS
@@ -43,8 +44,7 @@ from cqa_model import *
 from cqa_gen_batches import *
 
 from scorer import external_call # quac official evaluation script
-CURRENT_EPOCH = 0
-EPOCHS = 2
+
 
 # In[4]:
 
@@ -102,6 +102,15 @@ if FLAGS.do_train:
                                         max_query_length=FLAGS.max_query_length, 
                                         max_considered_history_turns=FLAGS.max_considered_history_turns, 
                                         is_training=True)
+        with open(features_fname, 'wb') as handle:
+            pickle.dump(train_features, handle)
+        with open(example_tracker_fname, 'wb') as handle:
+            pickle.dump(example_tracker, handle)
+        with open(variation_tracker_fname, 'wb') as handle:
+            pickle.dump(variation_tracker, handle)     
+        with open(example_features_nums_fname, 'wb') as handle:
+            pickle.dump(example_features_nums, handle) 
+        print('train features generated')
                 
     
     num_train_steps = FLAGS.train_steps
@@ -235,125 +244,139 @@ with tf.Session() as sess:
         heq_list = []
         dheq_list = []
         
-        # Training cycle
-        step = 0
-        while True:
-            step += 1
-            if step > num_train_steps:
-                # this means the learning rate has been decayed to 0
-                break
+        current_file = 1
+        num_files = 10
+        while current_file <= num_files:
+            with open('/data/all_features_{}'.format(current_file),'rb') as file_:
+                train_features = pickle.load(file_)
+            with open('/data/example_tracker_{}'.format(current_file),'rb') as file_:
+                example_tracker = pickle.dump(file_)
+            with open('/data/variation_tracker_{}'.format(current_file),'rb') as file_:
+                variation_tracker = pickle.dump(file_)
+            with open('/data/example_features_nums_{}'.format(current_file),'rb') as file_:
+                example_features_nums = pickle.load(file_)
+
+            train_batches = cqa_gen_example_aware_batches(train_features, example_tracker, variation_tracker, 
+                                          example_features_nums, FLAGS.train_batch_size, 
+                                          FLAGS.num_train_epochs, shuffle=False)
             
-            train_batches = cqa_gen_example_aware_batches(FLAGS.train_batch_size, shuffle=False)
-            batch_features, batch_example_tracker, batch_variation_tracker = batch
-            
+            current_file += 1
+            # Training cycle
+            for step, batch in enumerate(train_batches):
+                if step > num_train_steps:
+                    # this means the learning rate has been decayed to 0
+                    break
 
-            selected_example_features, relative_selected_pos = get_selected_example_features_without_actions(
-                                                    batch_features, batch_example_tracker, batch_variation_tracker)
+                batch_features, batch_example_tracker, batch_variation_tracker = batch
 
-            fd = convert_features_to_feed_dict(selected_example_features) # feed_dict
-            try:
-                _, train_summary, total_loss_res = sess.run([train_op, merged_summary_op, total_loss], 
-                                           feed_dict={unique_ids: fd['unique_ids'], input_ids: fd['input_ids'], 
-                                           input_mask: fd['input_mask'], segment_ids: fd['segment_ids'], 
-                                           start_positions: fd['start_positions'], end_positions: fd['end_positions'], 
-                                           history_answer_marker: fd['history_answer_marker'], training: True})
-            except:
-                print('features length: ', len(selected_example_features))
 
-            train_summary_writer.add_summary(train_summary, step)
-            train_summary_writer.flush()
-            print('training step: {}, total_loss: {}'.format(step, total_loss_res))
-            
-            if step >= FLAGS.evaluate_after and step % FLAGS.evaluation_steps == 0 and step != 0:
-                val_total_loss = []
-                all_results = []
-                all_selected_examples = []
-                all_selected_features = []
-                
-                total_num_selected = 0
-                total_num_actions = 0
-                total_num_examples = 0
-                
-                val_batches = cqa_gen_example_aware_batches(val_features, val_example_tracker, val_variation_tracker, 
-                                           val_example_features_nums, FLAGS.predict_batch_size, 1, shuffle=False)
-                
-                for val_batch in val_batches:
+                selected_example_features, relative_selected_pos = get_selected_example_features_without_actions(
+                                                        batch_features, batch_example_tracker, batch_variation_tracker)
 
-                    batch_results = []
-                    batch_features, batch_example_tracker, batch_variation_tracker = val_batch
-                    
-                    selected_example_features, relative_selected_pos = get_selected_example_features_without_actions(
-                                                    batch_features, batch_example_tracker, batch_variation_tracker)
+                fd = convert_features_to_feed_dict(selected_example_features) # feed_dict
+                try:
+                    _, train_summary, total_loss_res = sess.run([train_op, merged_summary_op, total_loss], 
+                                               feed_dict={unique_ids: fd['unique_ids'], input_ids: fd['input_ids'], 
+                                               input_mask: fd['input_mask'], segment_ids: fd['segment_ids'], 
+                                               start_positions: fd['start_positions'], end_positions: fd['end_positions'], 
+                                               history_answer_marker: fd['history_answer_marker'], training: True})
+                except:
+                    print('features length: ', len(selected_example_features))
 
-                        
-                    try:
-                        all_selected_features.extend(selected_example_features)
+                train_summary_writer.add_summary(train_summary, step)
+                train_summary_writer.flush()
+                print('training step: {}, total_loss: {}'.format(step, total_loss_res))
 
-                        fd = convert_features_to_feed_dict(selected_example_features) # feed_dict
-                        start_logits_res, end_logits_res, batch_total_loss = sess.run([start_logits, end_logits, total_loss], 
-                                    feed_dict={unique_ids: fd['unique_ids'], input_ids: fd['input_ids'], 
-                                    input_mask: fd['input_mask'], segment_ids: fd['segment_ids'], 
-                                    start_positions: fd['start_positions'], end_positions: fd['end_positions'], 
-                                    history_answer_marker: fd['history_answer_marker'], training: False})
+                if step >= FLAGS.evaluate_after and step % FLAGS.evaluation_steps == 0 and step != 0:
+                    val_total_loss = []
+                    all_results = []
+                    all_selected_examples = []
+                    all_selected_features = []
 
-                        val_total_loss.append(batch_total_loss)
+                    total_num_selected = 0
+                    total_num_actions = 0
+                    total_num_examples = 0
 
-                        for each_unique_id, each_start_logits, each_end_logits in zip(fd['unique_ids'], start_logits_res, 
-                                                                                      end_logits_res):  
-                            each_unique_id = int(each_unique_id)
-                            each_start_logits = [float(x) for x in each_start_logits.flat]
-                            each_end_logits = [float(x) for x in each_end_logits.flat]
-                            batch_results.append(RawResult(unique_id=each_unique_id, start_logits=each_start_logits, 
-                                                           end_logits=each_end_logits))
+                    val_batches = cqa_gen_example_aware_batches(val_features, val_example_tracker, val_variation_tracker, 
+                                               val_example_features_nums, FLAGS.predict_batch_size, 1, shuffle=False)
 
-                        all_results.extend(batch_results)
-                    except:
-                        print('batch dropped because too large!')
+                    for val_batch in val_batches:
 
-                output_prediction_file = os.path.join(FLAGS.output_dir, "predictions_{}.json".format(step))
-                output_nbest_file = os.path.join(FLAGS.output_dir, "nbest_predictions_{}.json".format(step))
+                        batch_results = []
+                        batch_features, batch_example_tracker, batch_variation_tracker = val_batch
 
-                write_predictions(val_examples, all_selected_features, all_results,
-                                  FLAGS.n_best_size, FLAGS.max_answer_length,
-                                  FLAGS.do_lower_case, output_prediction_file,
-                                  output_nbest_file)
+                        selected_example_features, relative_selected_pos = get_selected_example_features_without_actions(
+                                                        batch_features, batch_example_tracker, batch_variation_tracker)
 
-                val_total_loss_value = np.average(val_total_loss)
-                                
-                
-                # call the official evaluation script
-                val_summary = tf.Summary() 
-                val_eval_res = external_call(val_file_json, output_prediction_file)
 
-                val_f1 = val_eval_res['f1']
-                val_followup = val_eval_res['followup']
-                val_yesno = val_eval_res['yes/no']
-                val_heq = val_eval_res['HEQ']
-                val_dheq = val_eval_res['DHEQ']
+                        try:
+                            all_selected_features.extend(selected_example_features)
 
-                heq_list.append(val_heq)
-                dheq_list.append(val_dheq)
+                            fd = convert_features_to_feed_dict(selected_example_features) # feed_dict
+                            start_logits_res, end_logits_res, batch_total_loss = sess.run([start_logits, end_logits, total_loss], 
+                                        feed_dict={unique_ids: fd['unique_ids'], input_ids: fd['input_ids'], 
+                                        input_mask: fd['input_mask'], segment_ids: fd['segment_ids'], 
+                                        start_positions: fd['start_positions'], end_positions: fd['end_positions'], 
+                                        history_answer_marker: fd['history_answer_marker'], training: False})
 
-                val_summary.value.add(tag="followup", simple_value=val_followup)
-                val_summary.value.add(tag="val_yesno", simple_value=val_yesno)
-                val_summary.value.add(tag="val_heq", simple_value=val_heq)
-                val_summary.value.add(tag="val_dheq", simple_value=val_dheq)
+                            val_total_loss.append(batch_total_loss)
 
-                print('evaluation: {}, total_loss: {}, f1: {}, followup: {}, yesno: {}, heq: {}, dheq: {}\n'.format(
-                    step, val_total_loss_value, val_f1, val_followup, val_yesno, val_heq, val_dheq))
-                with open(FLAGS.output_dir + 'step_result.txt', 'a') as fout:
-                        fout.write('{},{},{},{},{},{}\n'.format(step, val_f1, val_heq, val_dheq, 
-                                            FLAGS.history, FLAGS.output_dir))
-                
-                val_summary.value.add(tag="total_loss", simple_value=val_total_loss_value)
-                val_summary.value.add(tag="f1", simple_value=val_f1)
-                f1_list.append(val_f1)
-                
-                val_summary_writer.add_summary(val_summary, step)
-                val_summary_writer.flush()
-                
-                save_path = saver.save(sess, '{}/model_{}.ckpt'.format(FLAGS.output_dir, step))
-                print('Model saved in path', save_path)
+                            for each_unique_id, each_start_logits, each_end_logits in zip(fd['unique_ids'], start_logits_res, 
+                                                                                          end_logits_res):  
+                                each_unique_id = int(each_unique_id)
+                                each_start_logits = [float(x) for x in each_start_logits.flat]
+                                each_end_logits = [float(x) for x in each_end_logits.flat]
+                                batch_results.append(RawResult(unique_id=each_unique_id, start_logits=each_start_logits, 
+                                                               end_logits=each_end_logits))
+
+                            all_results.extend(batch_results)
+                        except:
+                            print('batch dropped because too large!')
+
+                    output_prediction_file = os.path.join(FLAGS.output_dir, "predictions_{}.json".format(step))
+                    output_nbest_file = os.path.join(FLAGS.output_dir, "nbest_predictions_{}.json".format(step))
+
+                    write_predictions(val_examples, all_selected_features, all_results,
+                                      FLAGS.n_best_size, FLAGS.max_answer_length,
+                                      FLAGS.do_lower_case, output_prediction_file,
+                                      output_nbest_file)
+
+                    val_total_loss_value = np.average(val_total_loss)
+
+
+                    # call the official evaluation script
+                    val_summary = tf.Summary() 
+                    val_eval_res = external_call(val_file_json, output_prediction_file)
+
+                    val_f1 = val_eval_res['f1']
+                    val_followup = val_eval_res['followup']
+                    val_yesno = val_eval_res['yes/no']
+                    val_heq = val_eval_res['HEQ']
+                    val_dheq = val_eval_res['DHEQ']
+
+                    heq_list.append(val_heq)
+                    dheq_list.append(val_dheq)
+
+                    val_summary.value.add(tag="followup", simple_value=val_followup)
+                    val_summary.value.add(tag="val_yesno", simple_value=val_yesno)
+                    val_summary.value.add(tag="val_heq", simple_value=val_heq)
+                    val_summary.value.add(tag="val_dheq", simple_value=val_dheq)
+
+                    print('evaluation: {}, total_loss: {}, f1: {}, followup: {}, yesno: {}, heq: {}, dheq: {}\n'.format(
+                        step, val_total_loss_value, val_f1, val_followup, val_yesno, val_heq, val_dheq))
+                    with open(FLAGS.output_dir + 'step_result.txt', 'a') as fout:
+                            fout.write('{},{},{},{},{},{}\n'.format(step, val_f1, val_heq, val_dheq, 
+                                                FLAGS.history, FLAGS.output_dir))
+
+                    val_summary.value.add(tag="total_loss", simple_value=val_total_loss_value)
+                    val_summary.value.add(tag="f1", simple_value=val_f1)
+                    f1_list.append(val_f1)
+
+                    val_summary_writer.add_summary(val_summary, step)
+                    val_summary_writer.flush()
+
+                    save_path = saver.save(sess, '{}/model_{}.ckpt'.format(FLAGS.output_dir, step))
+                    print('Model saved in path', save_path)
 
                 
 
