e4563b63bc1915515a60f178d74f5600cab0ce74
Lewbrino <joespider885@yahoo.com>
2021-09-11 17:09:22 +0430

    Update cqa_supports.py

--- cqa_supports.py
+++ cqa_supports.py
@@ -841,8 +841,8 @@ def convert_examples_to_variations_and_then_features(examples, tokenizer, max_se
     variation_tracker = []
     # matching_signals_dict = {}
     unique_id = 1000000000
-    num_files_train = 60
-    num_files_val = 20
+    num_files_train = 200
+    num_files_val = 40
     features_num_train = 510320
     features_num_val = 81518
     each_file_features_train = int(features_num_train // num_files_train)
